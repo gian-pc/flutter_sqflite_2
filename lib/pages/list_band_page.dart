f7cf5f5c50dfc218b6700dcfe662453af68216bc
Gian Carlos <gpaucarcortez@gmail.com>
2021-10-26 17:01:02 -0500

    Init Band

---
+++ lib/pages/list_band_page.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class ListBandPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ListBandPage"),
+      ),
+      body: Center(),
+    );
+  }
+}
