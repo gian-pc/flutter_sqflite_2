9d0bdbd19cddf8e538c40ce9a7761efab7b92023
Gian Carlos <gpaucarcortez@gmail.com>
2021-10-26 18:38:43 -0500

    Init List Band

--- lib/pages/list_band_page.dart
+++ lib/pages/list_band_page.dart
@@ -1,15 +1,49 @@
-
 import 'package:flutter/material.dart';
 
 class ListBandPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("ListBandPage"),
+      backgroundColor: Color(0xff282A3C),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Gian PC",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      Text(
+                        "Bienvenido",
+                        style: TextStyle(
+                          color: Colors.white38,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Text(
+                "Mis bandas",
+                style: TextStyle(color: Colors.white, fontSize: 32.0),
+              )
+            ],
+          ),
+        ),
       ),
-      body: Center(),
     );
   }
 }
