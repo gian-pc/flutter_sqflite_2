27c817411698716d7d26e1dcef22456580819f3f
Gian Carlos <gpaucarcortez@gmail.com>
2021-10-27 09:30:58 -0500

    Init DB

--- lib/db/db_global.dart
+++ lib/db/db_global.dart
@@ -1,4 +1,21 @@
+import 'package:sqflite/sqflite.dart';
 
-class DBGlobal{
+class DBGlobal {
+  static Database? _myDatabase;
+  static final DBGlobal db = DBGlobal._();
+  DBGlobal._();
 
+  //Obtener o crear la base de datos
+
+  Future<Database?> get getDatabase async {
+    if (_myDatabase != null) return _myDatabase;
+    _myDatabase = await initDB();
+    return _myDatabase;
+  }
+
+  //Creación de la base de datos y las tablas
+
+  initDB() async {
+
+  }
 }
