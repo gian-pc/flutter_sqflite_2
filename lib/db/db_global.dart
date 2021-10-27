fe189a46dd9c34db4737d699179a7cc3c0b8bee7
Gian Carlos <gpaucarcortez@gmail.com>
2021-10-27 09:47:19 -0500

    Init DB 2

--- lib/db/db_global.dart
+++ lib/db/db_global.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBGlobal {
@@ -16,6 +20,17 @@ class DBGlobal {
   //Creación de la base de datos y las tablas
 
   initDB() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentDirectory.path, "BandDB.db");
+    return openDatabase(
+      path,
+      version: 1,
+      onOpen: (db){},
+      onCreate: (Database db, int version){
+        db.execute("CREATE TABLE Band(id INTEGER PRIMARY KEY AUTOINCREMENT, bandName TEXT, status TEXT,favorite TEXT)");
+        
+      }
+    );
 
   }
 }
